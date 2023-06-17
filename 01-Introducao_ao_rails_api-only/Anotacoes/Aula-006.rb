e27cb7f10f79c968e3762917dec9f94df4c488a1
Rodrigo Adelino <rodrigoadelino@outlook.com>
2023-06-17 08:46:02 -0300

    Secao_1-Aula06

---
+++ 01-Introducao_ao_rails_api-only/Anotacoes/Aula-006.rb
@@ -0,0 +1,12 @@
+# Site do Culr
+https://curl.se/
+
+# Instalar o Curl no Linux (ubuntu)
+sudo apt-get install curl
+
+# ver a versão instalada
+curl --version
+
+# get em contacts (Funciona no windows 11)
+curl http://127.0.0.1:3000/contacts
+
