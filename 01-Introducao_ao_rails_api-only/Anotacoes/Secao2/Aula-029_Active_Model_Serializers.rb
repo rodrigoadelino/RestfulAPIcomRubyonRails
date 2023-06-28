024bdec69a038787a12a4b31cfc7efd694300063
Rodrigo Adelino <rodrigoadelino@outlook.com>
2023-06-28 08:06:43 -0300

    Secao2-Aula29

---
+++ 01-Introducao_ao_rails_api-only/Anotacoes/Secao2/Aula-029_Active_Model_Serializers.rb
@@ -0,0 +1,6 @@
+## Instalar a gem
+gem 'active_model_serializers', '~>0.10.0'
+
+#Criar um serializer para ps models contact e kind serao criado em: notebook-api\app\serializers
+rails g serializer contact
+rails g serializer kind
