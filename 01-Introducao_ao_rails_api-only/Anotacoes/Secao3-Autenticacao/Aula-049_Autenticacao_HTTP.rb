13b83e0f0b57f6e7b6c341968e6f639f600cb7cb
Rodrigo Bonfim <rodrigobonfim@singulare.com.br>
2023-07-10 17:19:21 -0300

    Secao3-Aula49

---
+++ 01-Introducao_ao_rails_api-only/Anotacoes/Secao3-Autenticacao/Aula-049_Autenticacao_HTTP.rb
@@ -0,0 +1,15 @@
+### no IRB
+
+# Criptografar com BASE64
+require 'base64'
+Base64.encode64('rodrigo:senha') # Coloca o enter no final \n
+Base64.strict_encode64('rodrigo:senha') # Não coloca o enter no final \n
+
+
+# Criptografar com MD5
+require 'digest/md5'
+Digest::MD5::hexdigest('rodrigo:senha')
+
+
+# Fazer o login usando usuario e senha com o Curl 
+curl http://localhost:3000/kinds -I -H 'Accept: application/json' -u rodrigo:secret
