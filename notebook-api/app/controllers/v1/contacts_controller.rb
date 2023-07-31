69d8334520b697d86cacd91ab3759f2b48e4440a
Rodrigo Adelino <rodrigoadelino@outlook.com>
2023-07-31 18:52:01 -0300

    Secao7-Aula73

--- notebook-api/app/controllers/v1/contacts_controller.rb
+++ notebook-api/app/controllers/v1/contacts_controller.rb
@@ -10,7 +10,13 @@ module V1
       @contacts = Contact.all.page(page_number).per(per_page) 
       # # Cache-control
       # expires_in 30.seconds, public: true 
-      render json: @contacts
+      # if stale?(etag: @contacts)
+      #   render json: @contacts
+      # end
+      #Last Modifield
+      if stale?(last_modified: @contacts[0].updated_at)
+        render json: @contacts
+      end
     end
 
     # GET /contacts/1
