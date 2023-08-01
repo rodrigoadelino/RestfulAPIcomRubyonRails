b3dec7d4cd51431b8925d04ce30bd314b8a45f6c
Rodrigo Adelino <rodrigoadelino@outlook.com>
2023-08-01 06:19:23 -0300

    Secao8_Aula74

--- 01-Introducao_ao_rails_api-only/Anotacoes/Secao7-Caching/Aula-073_Etag_e_last_modified.rb
+++ 01-Introducao_ao_rails_api-only/Anotacoes/Secao7-Caching/Aula-073_Etag_e_last_modified.rb
@@ -1,1 +1,38 @@
-## 
+## Incluir a verificacao da Etag em: notebook-api\app\controllers\v1\contacts_controller.rb
+# GET /contacts
+def index
+    page_number = params[:page].try(:[], :number)
+    per_page = params[:page].try(:[], :size)
+    @contacts = Contact.all.page(page_number).per(per_page) 
+    # # Cache-control
+    # expires_in 30.seconds, public: true 
+    if stale?(etag: @contacts)
+        render json: @contacts
+    end
+end
+
+
+### Pegar a etag que vem no headers do postam e renviar como:
+if-None-Match :  "Etag"
+
+
+## Incluir a verificacao da Etag em: notebook-api\app\controllers\v1\contacts_controller.rb 
+# GET /contacts
+def index
+    page_number = params[:page].try(:[], :number)
+    per_page = params[:page].try(:[], :size)
+    @contacts = Contact.all.page(page_number).per(per_page) 
+    # # Cache-control
+    # expires_in 30.seconds, public: true 
+    # if stale?(etag: @contacts)
+    #   render json: @contacts
+    # end
+    #Last Modifield
+    if stale?(last_modified: @contacts[0].updated_at)
+        render json: @contacts
+    end
+end
+
+
+### Pegar a Last-Modified que vem no header e passa com oparametro em 
+If-Modified-Since:  "Last-Modified"
